cf945c16a7a617f8f08bb11fa5f3b4cd9bde5e01
hrong <1103749761@qq.com>
2018-03-04 21:32:16 +0800

    退出登录模态框完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -18,6 +18,61 @@ $(function(){
         setTimeout(function(){
             NProgress.done();
         },500)
-    })
-    
+    });
+
+
+    // 二级菜单显示与隐藏
+    // 找前一个兄弟元素
+    $('.second').prev().on('click',function(){
+        // 显示和隐藏状态之间切换
+        $(this).next().slideToggle();
+    });
+
+
+    // 侧边栏显示隐藏
+    $('.icon_menu').on('click',function(){
+        $('.lt_aside').toggleClass('now');
+        // margin-left 为0；添加动画
+        $('.lt_main').toggleClass('now');
+    });
+
+
+    // 退出登录
+    $('.icon_logout').on('click',function(){
+        // $('#Mymodal').modal({
+        //     keyboard: false
+        //   })
+        // 让模态框显示
+        $('#Mymodal').modal('show');
+    });
+
+    // 模态框确定退出
+    $('.btn_logout').on('click',function(){
+        $.ajax({
+            type:'get',
+            url:'/employee/employeeLogout',
+            success:function(info){
+                if(info.success){
+                    //退出成功，才跳转到登录页
+                    location.href = 'login.html';
+                }
+            }
+        });
+    });
+
+
+    // 不是登陆页，发送ajax请求，查询管理员是否登录
+    if(location.href.indexOf('login.html') == -1 ){
+        $.ajax({
+            type:'get',
+            url:'/employee/checkRootLogin',
+            success:function(info){
+                // 如果未登录就跳出到登录页
+                if(info.error == 400 ) {
+                    location.href = 'login.html';
+                }
+            }
+        })
+    }
+
 });
