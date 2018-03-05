8a185e472be8f40ed1e33b052f20c3292cd83cca
hrong <1103749761@qq.com>
2018-03-05 18:38:19 +0800

    用户管理完成

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -12,27 +12,54 @@ $(function(){
             },
             dataType:'json',
             success:function (info) {
-                console.log(info);
                 $('.table tbody').html(template('tpl',info));
                 
                 // 渲染分页
-                $('#paginator').bootstrapPaginator({
-                    bootstrapMajorVersion:3, //如果使用了bootstrap3版本，必须指定
-                    currentPage: page,  //设置当前页
-                    totalPages: Math.ceil(info.total/info.size),//设置总页数
-                    numberOfPages:5,// 设置显示多少页
-                    //当页码被点击的时候触发
-                    onPageClicked: function (a,b,c,p) {
-                      //修改一下page的值
-                      page = p;
-                      //重新渲染
-                      render();
-                    }
-                });
+                //6. 渲染分页
+            $("#paginator").bootstrapPaginator({
+                bootstrapMajorVersion:3,//boostrap使用版本 2使用div元素，3使用ul
+                currentPage:page, //设置当前页
+                numberOfPages: 5, //设置控件显示的页码数
+                totalPages:Math.ceil(info.total / info.size),
+                onPageClicked:function(a,b,c,p){
+                //绑定clck事件
+                page = p;
+                render();
+                }
+            });
             }
         })
     }
     render();
 
-    
+    // 启用禁用用户
+    $('tbody').on('click','.btn',function(){
+        // 显示模态框
+        $("#UserModal").modal("show");
+
+        //获取到点击的按钮所在的用户的id
+        var id = $(this).parent().data('id');
+        var isDelete = $(this).hasClass('btn-success') ? 1 : 0;
+
+        $('.btn_confim').off().on('click',function(){
+            $.ajax({
+                type:"POST",
+                url:'/user/updateUser',
+                data:{
+                    id:id,
+                    isDelete:isDelete
+                },
+                success:function(info){
+                    if(info.success) {
+                        // 关闭模态框
+                        $("#UserModal").modal("hide");
+                        // 重新渲染
+                        render();
+                    }
+                }
+            });
+        })
+    });
+
+
 }); 
