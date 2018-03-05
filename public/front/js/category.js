f896bb7b10250eb5959dcba265dc36fd02967b2c
hrong <1103749761@qq.com>
2018-03-05 22:33:16 +0800

    分类页完成

--- public/front/js/category.js
+++ public/front/js/category.js
@@ -1,9 +1,35 @@
 $(function(){
+  // 一级分类
     $.ajax({
         type:'GET',
         url:'/category/queryTopCategory',
         success:function (info) {
-          console.log(info);
+          $('.top').html(template('navTpl',info));
+          console.log(info.rows[0].id);
+
+          // 渲染二级分类
+          randerSecond(info.rows[0].id);
         }
       });
+
+  $('.top').on('click','li',function(){
+    $(this).addClass('now').siblings().removeClass('now');
+    id = $(this).data('id');
+    randerSecond(id)
+  });
+
+  // 二级分类
+  function randerSecond(id){
+    $.ajax({
+      type:'get',
+      url:"/category/querySecondCategory",
+      data:{
+        id: id 
+      },
+      success:function(info){
+        console.log(info)
+        $('.second').html(template('secondTpl',info));
+      }
+    });
+  }
 });
