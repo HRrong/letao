bff7e2bf869ba61a8db5050571b0ca1be2d1ce2b
hrong <1103749761@qq.com>
2018-03-06 15:58:43 +0800

    二级分类静态

--- public/back/js/second.js
+++ public/back/js/second.js
@@ -1,6 +1,14 @@
 $(function(){
     $('.addBtn').on('click',function(){
-        console.log(1)
         $("#addModal").modal('show');
+    });
+
+    $('#fileupload').fileupload({
+        dataType:'json',
+        // e：事件对象
+        // data:图片上传的对象，通过e.result.picAddr可以获取上传后的图片地址
+        done:function(e,date){
+            console.log(date);
+        }
     });
 });
